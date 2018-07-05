c535d8aef227758c4ecc979b2fdc9254d95cd075
Akari M <68madam.ma@gmail.com>
2018-07-05 18:19:05 +0900

    Add files via upload

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+# this allows the server to be accessed more widely
+# ある1つの防御を無効にするには、protectionにハッシュでオプションを指定します。
+#set :protection, :except => :frame_options
+set :environment, :production
+set :bind, "0.0.0.0"
+
+get '/' do
+  erb :index
+end
---
+++ js/index.js
@@ -0,0 +1,7 @@
+var request = new XMLHttpRequest();
+
+// open the request
+request.open('GET', 'https://flickr-images.herokuapp.com');
+
+// send the request
+request.send();
