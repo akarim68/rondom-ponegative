5b8c52001a3ebfc752abb7a48a1a7022150a348e
Akari M <68madam.ma@gmail.com>
2018-07-05 18:35:11 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -9,6 +9,6 @@ set :bind, "0.0.0.0"
 get '/' do
   # this allows requests from other servers
   response['Access-Control-Allow-Origin'] = '*'
-  response['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+  #response['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
   erb :index
 end
